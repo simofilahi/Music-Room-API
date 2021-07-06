62f332a48e32c956cfb3873f210165cc277cd08b
Mohamed Filahi <mohamedfilahi222@gmail.com>
2021-07-06 15:18:19 +0100

    facebook oauth added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
 import "./App.css";
 import axios from "axios";
 import { GoogleLogin } from "react-google-login";
+import FacebookLogin from "react-facebook-login";
 
 const responseGoogle = async (response) => {
   const data = await axios.post("http://localhost:4004/api/auth/google", {
-    idToken: response.tokenId,
+    accessToken: response.accessToken,
   });
   console.log(data);
 };
 
+const responseFacebook = async (response) => {
+  const data = await axios.post("http://localhost:4004/api/auth/facebook", {
+    accessToken: response.accessToken,
+  });
+  console.log(response);
+};
+
 function App() {
   return (
-    <GoogleLogin
-      clientId={
-        "530974951834-mivmchdoqe7cm2uagje58hcce1dg07pg.apps.googleusercontent.com"
-      }
-      buttonText="Log in with Google"
-      onSuccess={responseGoogle}
-      // onFailure={handleLogin}
-      cookiePolicy={"single_host_origin"}
-    />
+    <div>
+      <div>
+        <GoogleLogin
+          clientId={
+            "530974951834-mivmchdoqe7cm2uagje58hcce1dg07pg.apps.googleusercontent.com"
+          }
+          buttonText="Log in with Google"
+          onSuccess={responseGoogle}
+          // onFailure={handleLogin}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
+      <div>
+        <FacebookLogin
+          appId="563098104694816"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+      </div>
+    </div>
   );
 }
 
